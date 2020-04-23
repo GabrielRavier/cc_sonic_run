069e6228ba9b6e6542b2e1119d29096303fc70a8
Gabriel Ravier <gabravier@gmail.com>
2020-04-23 15:36:08 +0200

    Did very basic thing where player can move left or right
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- sonic.py
+++ sonic.py
@@ -22,6 +22,24 @@ class Player(pygame.sprite.Sprite):
     speed = 10
     images = []
 
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect(midbottom = SCREENRECT.midbottom)
+        self.origtop = self.rect.top
+        self.facing = -1
+
+    def move(self, direction):
+        if direction:
+            self.facing = direction
+
+        self.rect.move_ip(direction * self.speed, 0)
+        self.rect = self.rect.clamp(SCREENRECT)
+        if direction > 0:
+            self.image = self.images[0]
+        elif direction < 0:
+            self.image = self.images[1]
+
 def main():
     # Initialize pygame
     pygame.init()
@@ -31,7 +49,8 @@ def main():
     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
 
     # Load images for player
-
+    img = loadImage('Sonic.png')
+    Player.images = [img, pygame.transform.flip(img, 1, 0)]
 
     # Decorate window
     pygame.display.set_caption('Pygame Sonic Run')
@@ -45,6 +64,38 @@ def main():
 
     screen.blit(background, (0, 0))
     pygame.display.flip()
+
+    all = pygame.sprite.RenderUpdates()
+    Player.containers = all
+
+    player = Player()
+    clock = pygame.time.Clock()
+
+    # Main loop
+    while True:
+        # Get input
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                return
+
+        keystate = pygame.key.get_pressed()
+
+        # Clear the last drawn sprites
+        all.clear(screen, background)
+
+        # Update all the sprites
+        all.update()
+
+        # Handle player input
+        direction = keystate[K_RIGHT] - keystate[K_LEFT]
+        player.move(direction)
+
+        # Draw the scene
+        dirty = all.draw(screen)
+        pygame.display.update(dirty)
+
+        # Cap the framerate
+        clock.tick(40)
 
     pygame.time.wait(1000)
     pygame.quit()
